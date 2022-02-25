0db7b731d6a4b429fa66ca28e7cc32ad83757df7
Evnur-ctnky1 <90348760+Evnur-ctnky1@users.noreply.github.com>
2022-02-25 21:14:12 +0300

    Add files via upload

---
+++ kodlar.js
@@ -0,0 +1,81 @@
+function merhaba(){
+    let ad,soyad,yas,burc;
+    ad=document.getElementById("txtAd").value;
+    yas=document.getElementById("txtSoyad").value;
+    burc=document.getElementById("txtburc").selecedIndex;
+    console.log(burc);
+    document.getElementById("sonucbaslık").innerHTML="";
+    document.getElementById("yazi").innerHTML="";
+}
+if(burc==1){
+    document.body.style.backgroundColor="#000";
+    yazi.innerHTML="BURCUNUZ OĞLAK =)"
+   document.getElementById("İMAGES").setAttribute("src","")
+
+}
+if(burc==2){
+    document.body.style.backgroundColor="#000";
+    yazi.innerHTML="BURCUNUZ KOVA =)"
+   document.getElementById("İMAGES").setAttribute("src","")
+
+}
+if(burc==3){
+    document.body.style.backgroundColor="#000";
+    yazi.innerHTML="BURCUNUZ BALIK =)"
+   document.getElementById("İMAGES").setAttribute("src","")
+
+}
+if(burc==4){
+    document.body.style.backgroundColor="#000";
+    yazi.innerHTML="BURCUNUZ KOÇ =)"
+   document.getElementById("İMAGES").setAttribute("src","")
+
+}
+if(burc==5){
+    document.body.style.backgroundColor="#000";
+    yazi.innerHTML="BURCUNUZ BOĞA =)"
+   document.getElementById("İMAGES").setAttribute("src","")
+
+}
+if(burc==6){
+    document.body.style.backgroundColor="#000";
+    yazi.innerHTML="BURCUNUZ İKİZLER =)"
+   document.getElementById("İMAGES").setAttribute("src","")
+
+}
+if(burc==7){
+    document.body.style.backgroundColor="#000";
+    yazi.innerHTML="BURCUNUZ YENGEÇ =)"
+   document.getElementById("İMAGES").setAttribute("src","")
+
+}
+if(burc==8){
+    document.body.style.backgroundColor="#000";
+    yazi.innerHTML="BURCUNUZ ASLAN =)"
+   document.getElementById("İMAGES").setAttribute("src","")
+
+}
+if(burc==9){
+    document.body.style.backgroundColor="#000";
+    yazi.innerHTML="BURCUNUZ BAŞAK =)"
+   document.getElementById("İMAGES").setAttribute("src","")
+
+}
+if(burc==10){
+    document.body.style.backgroundColor="#000";
+    yazi.innerHTML="BURCUNUZ TERAZİ =)"
+   document.getElementById("İMAGES").setAttribute("src","")
+
+}
+if(burc==11){
+    document.body.style.backgroundColor="#000";
+    yazi.innerHTML="BURCUNUZ AKREP =)"
+   document.getElementById("İMAGES").setAttribute("src","")
+
+}
+if(burc==12){
+    document.body.style.backgroundColor="#000";
+    yazi.innerHTML="BURCUNUZ YAY =)"
+   document.getElementById("İMAGES").setAttribute("src","")
+
+}
